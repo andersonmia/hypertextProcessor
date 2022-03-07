5c892b433fa67bd3c49a8e032bb700e5aebe382c
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-07 11:41:11 -0800

    connect to db

---
+++ getForm.php
@@ -0,0 +1,35 @@
+<?php 
+$firstname = $_GET['firstname'];
+$lastname = $_GET['lastname'];
+$email = $_GET['email'];
+$telephone = $_GET['telephone'];
+$nationality = $_GET['nationality'];
+$gender = $_GET['gender'];
+$username = $_GET['username'];
+$password = $_GET['password'];
+$cpassword = $_GET['cpassword'];
+
+if(($password!=$cpassword)||($firstname=="")||($lastname=="")||($email=="")||($username=="")||($gender=="")||($nationality=="")||($telephone=="")){
+    echo "please refill in the details carefully";
+}else{
+    $validpassword = hash("SHA512",$password);
+    define("HOST","localhost")
+    $db_host = "localhost";
+    $db_name = "getform";
+    $db_user_name = "andersonmia";
+    $db_user_password = "&2005&";
+    $connect = mysqli(host,$db_name,$db_user_name);
+    if(!$connect){
+        echo mysqli_connect_error();
+    }else{
+        $insertQuery = "INSERT INTO signup(firstname,lastname,email,telephone,username,password,gender,nationality) VALUES('$firstname','$lastname','$email','$telephone','$username','$validpassword', '$gender', '$nationality')";
+        $insert = mysqli_query($connect,$insertQuery) or die("an error occured:" .mySqli_error($connect))
+        if($insert){
+            echo "You are fully registered";
+        }else{
+            echo "Registration failed";
+        }
+    }
+}
+?>
+ 
