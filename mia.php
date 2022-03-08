b77ed68d840f13cc7972802aac109947886084f5
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-08 01:37:15 -0800

    the often used code in both display.php and form.phpe to prevent re-writing the same codes

---
+++ mia.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST","localhost");
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="form";
+$connection= mysqli_connect($db_host,$db_user,$db_password,$db_name);
+?>
