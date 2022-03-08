9daaf7dd791a978c80269fe1d1e9c99d63c48511
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-08 01:37:47 -0800

    updated

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <?php
+include('./mia.php');
 $firstName=$_POST['fn'];
 $lastName=$_POST['ln'];
 $email=$_POST['email'];
@@ -10,24 +11,19 @@ $password=$_POST['pass'];
 $passConfirm=$_POST['pass2'];
 
 
-if(($firstName=="")||($lastName=="")||($email=="")||($password!=$passConfirm)){
+if(($firstName=="")||($lastName=="")||($email=="")||($password!=$passConfirm)||($password=="")){
     echo "Please provide all the required information";
 }else{
     $encrypt=hash("SHA512",$password);
-    define("HOST","localhost");
-    $db_host="localhost";
-    $db_user="root";
-    $db_password="";
-    $db_name="form";
-    $connection= mysqli_connect(HOST,$db_user,$db_password,$db_name);
+    
     if(!$connection){
         echo"Connection Error".mysqli_connect_error();
     }
     else{
-        $insertquery="INSERT INTO registration(firstname,lastname,gender,phoneNumber,email,nationality,username,password) values('$firstName','$lastName','$gender','$telephone','$email','$nationality','$username','$encrypt')";
+        $insertquery="INSERT INTO registration(firstname,lastname,gender,telephone,email,nationality,username,password) values('$firstName','$lastName','$gender','$telephone','$email','$nationality','$username','$encrypt')";
         $insert=mySqli_query($connection,$insertquery) or die("Error occured".mySqli_error($connection));
         if($insert){
-            echo "Data added succesfully";
+            echo "<h3>Data added succesfully<h3>";
         }else{
             "Failed to add a new user";
         }
